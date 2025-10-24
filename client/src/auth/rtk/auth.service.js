e508924f3bc29ab9c0cffb15a5c3293ea7db05f7
abhinandanchivate <abhinadanchivate2000@gmail.com>
2025-10-24 13:19:01 +0530

    feat(auth): add proxy setup for API calls in Vite configuration

--- client/src/auth/rtk/auth.service.js
+++ client/src/auth/rtk/auth.service.js
@@ -6,3 +6,7 @@
 // to add headers / logics
 // we will create functions for login / register / logout
 // to add auth token to headers or remove the token from headers
+// http://localhost:5000/api/auth/login ==> netwrok from the client side browser.
+// my customer will get to know that he is consuming xyz api from abc company.
+// it should look like everything is happening from my own frontend application only.
+// show like all api calls are happening from my own frontend application only but behind the scenes it should hit to my backend service ==> proxy setup
